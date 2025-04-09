b4be35178f18099de84d03fbf9a86ac06c511c50
MarianaLGM <mariana_lobeto@hotmail.com>
2025-04-09 13:34:27 +0200

    cambios

--- src/components/ReservarOnline.jsx
+++ src/components/ReservarOnline.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from 'react';
 
 const ReservarOnline = () => {
-    return (
-        <>
+        const [selectedDate, setSelectedDate] = useState(new Date());
+        return (
+            <>
             <div>
                 <nav>
                     <Link to='/inicio'>INICIO</Link>
@@ -18,10 +22,16 @@ const ReservarOnline = () => {
             </div>
             <div>
                 <h1>Reservar Online</h1>
-                <p></p>
+                <DatePicker
+                showIcon
+                toggleCalendarOnIconClick
+                selected={selectedDate}
+                onChange={(date) => setSelectedDate(date)}
+            />
             </div>
-        </>
-    );
-};
+            </>
+        );
+    };
+    
 
 export default ReservarOnline;
