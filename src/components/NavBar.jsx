5f0fd05c899dfb240775561dc8ecb7fadcfb26ee
MarianaLGM <mariana_lobeto@hotmail.com>
2025-04-11 15:58:13 +0200

    cambios

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,10 +11,6 @@ const NavBar = () => {
             <p className={styles.banner1}>¡15 AÑOS CUIDÁNDOTE! DISFRUTA DE UN 15% DE DESCUENTO EN TODOS NUESTROS TRATAMIENTOS</p>
         </div>
         <nav className={styles.navbar}>
-            <Link to='/inicio'>INICIO</Link>
-        </nav>   
-        
-        <nav>
             {!userLogged ? <Link to='/inicio'>INICIO</Link> : <Link to='/admin'>INICIO</Link>}
             <Link to='/tratamientos-faciales'>FACIAL</Link>
             <Link to='/tratamientos-corporales'>CORPORAL</Link>
@@ -31,37 +27,3 @@ const NavBar = () => {
 }
 
 export default NavBar;
-
-import { Link } from 'react-router-dom';
-import styles from '../styles/NavBar.module.css';
-
-import useLoggedUser from "../hooks/useLoggedUser"
-
-const NavBar = () => {
-    const {userLogged} = useLoggedUser();
-    return (
-        <>
-        <div className={styles.banner}>
-            <p className={styles.banner1}>¡15 AÑOS CUIDÁNDOTE! DISFRUTA DE UN 15% DE DESCUENTO EN TODOS NUESTROS TRATAMIENTOS</p>
-        </div>
-        <nav className={styles.navbar}>
-            <Link to='/inicio'>INICIO</Link>
-        </nav>   
-        <nav>
-            {!userLogged ? <Link to='/inicio'>INICIO</Link> : <Link to='/admin'>INICIO</Link>}
-            <Link to='/tratamientos-faciales'>FACIAL</Link>
-            <Link to='/tratamientos-corporales'>CORPORAL</Link>
-            <Link to='/acmella-oleracea'>ACMELLA OLERACEA</Link>
-            <Link to='/estrias'>ESTRÍAS</Link>
-            <Link to='/contacto'>CONTACTO</Link>
-            <Link to='/reservar-online'>RESERVAR ONLINE</Link>
-            <div className={styles.regalaTova}>
-            <Link to='/regala-tova' >REGALA TOVA</Link>
-            </div>
-        </nav>
-        </>
-    )
-}
-
-export default NavBar;
-
