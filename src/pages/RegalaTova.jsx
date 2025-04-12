e17eeeda0de90e6ffac19247e12cfbdc8d11ee49
MarianaLGM <mariana_lobeto@hotmail.com>
2025-04-12 20:37:36 +0200

    cambios

--- src/pages/RegalaTova.jsx
+++ src/pages/RegalaTova.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { useState } from 'react';
 import styles from '../styles/RegalaTova.module.css';
 
+
 const RegalaTova = () => {
+    const [formularioEnviado, setFormularioEnviado] = useState(false);
+
+
     const [formRegala, setFormRegala] = useState({
         importe: '',
         nombre: '',
@@ -14,86 +18,99 @@ const RegalaTova = () => {
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormRegala({ ...formRegala, [name]: value });
+        
     };
         
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        setFormularioEnviado(true)//mensaje envio ok despues de darle a enviar.
     };
+
+
     return (
         <>
-        <div className={styles.regala}>
-            <div className={styles.titulo}>
-                <h1>Regala Tova</h1>
-            </div>
-        </div>
-            <div className={styles.subtitulo}>
-                <p>Regala una experiencia totalmente personalizada para tu persona favorita.</p>
-                <p>Nosotros nos encargamos del resto.</p>   
+            <div className={styles.regala}>
+                <div className={styles.titulo}>
+                    <h1>Regala Tova</h1>
+                </div>
             </div>
 
-            <div className={styles.form}>
-                <form onSubmit={handleSubmit} >
-                    <div className={styles.importe}>
-                        <select
-                        id="importe"
-                        name="importe"
-                        value={formRegala.importe}
-                        onChange={handleChange}
-                        required>
-                            <option value="">Elige una opción</option>
-                            <option value={100}>100€</option>
-                            <option value={150}>150€</option>
-                            <option value={200}>200€</option>
-                            <option value={250}>250€</option>
-                        </select>
-                    </div>
-                    <div className={styles.nombre}>
-                        <label>Nombre y apellidos de la persona regalada:</label>
-                        <input
-                        type='text'
-                        id='nombre'
-                        name='nombre'
-                        value={formRegala.nombre}
-                        onChange={handleChange}
-                        required
-                        />
-                    </div>
-                    <div className={styles.dedicatoria}>
-                        <label>Escribe tu dedicatoria, si lo deseas:</label>
-                        <textarea
-                        id="mensaje"
-                        name="mensaje"
-                        value={formRegala.dedicatoria}
-                        onChange={handleChange}
-                        />
-                    </div>
+            {formularioEnviado ? (
+                // Si se ha enviado el formulario, muestra el mensaje de gracias
+                <p className={styles.mensajeGracias}> Gracias por confiar en Tova, en breves contactaremos contigo para que vengas a recoger tu tarjeta regalo a nuestra tienda.</p>
+            ) : (
+                // Si no se ha enviado, muestra el formulario
 
-                    <div className={styles.telefono}>
-                        <label>Teléfono:</label>
-                        <input 
-                        type='tel'
-                        id='telefono'
-                        name='telefono'
-                        value={formRegala.telefono}
-                        onChange={handleChange}
-                        required
-                        />
-                        <a>Inserta tu número de teléfono por si te tuviéramos que consultar algo</a>
-                    </div>
+                <div className={styles.form}>
+                    <form onSubmit={handleSubmit}>             
+                        <div className={styles.subtitulo}>
+                            <p>Regala una experiencia totalmente personalizada para tu persona favorita.</p>
+                            <p>Nosotros nos encargamos del resto.</p>
+                        </div>
+                        <div className={styles.importe}>
+                            <select
+                                id="importe"
+                                name="importe"
+                                value={formRegala.importe}
+                                onChange={handleChange}
+                                required
+                            >
+                                <option value="">Elige una opción</option>
+                                <option value={100}>100€</option>
+                                <option value={150}>150€</option>
+                                <option value={200}>200€</option>
+                                <option value={250}>250€</option>
+                            </select>
+                        </div>
+                        <div className={styles.nombre}>
+                            <label>Nombre y apellidos de la persona regalada:</label>
+                            <input
+                                type="text"
+                                id="nombre"
+                                name="nombre"
+                                value={formRegala.nombre}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className={styles.dedicatoria}>
+                            <label>Escribe tu dedicatoria, si lo deseas:</label>
+                            <textarea
+                                id="dedicatoria"
+                                name="dedicatoria"
+                                value={formRegala.dedicatoria}
+                                onChange={handleChange}
+                            />
+                        </div>
 
-                    <div className={styles.observaciones}>
-                        <textarea
-                        id="observaciones"
-                        name="observaciones"
-                        value={formRegala.observaciones}
-                        onChange={handleChange}
-                        />
-                        <a>Pregúntanos lo que quieras</a>
-                    </div>
-                    
-                    <button type="submit">Enviar</button>
-                </form>
-            </div>  
+                        <div className={styles.telefono}>
+                            <label>Teléfono:</label>
+                            <input
+                                type="tel"
+                                id="telefono"
+                                name="telefono"
+                                value={formRegala.telefono}
+                                onChange={handleChange}
+                                required
+                            />
+                            <a>Inserta tu número de teléfono por si te tuviéramos que consultar algo</a>
+                        </div>
+
+                        <div className={styles.observaciones}>
+                            <textarea
+                                id="observaciones"
+                                name="observaciones"
+                                value={formRegala.observaciones}
+                                onChange={handleChange}
+                            />
+                            <a>Pregúntanos lo que quieras</a>
+                        </div>
+
+                        <button type="submit">Enviar</button>
+                    </form>
+                </div>
+            )}
         </>
     );
 };
--- src/pages/ReservarOnline.jsx
+++ src/pages/ReservarOnline.jsx
@@ -2,124 +2,158 @@ import React from 'react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from 'react';
-import {getDay} from "date-fns";
+import { getDay } from "date-fns";
 import styles from '../styles/ReservarOnline.module.css';
+import { es } from 'date-fns/locale';
 
 const ReservarOnline = () => {
+    const [formularioEnviado, setFormularioEnviado] = useState(false);//para mostrar mensaje cuando se envía formulario
     const [startDate, setStartDate] = useState(null);
-    const [services, setServices]=useState({
-            servicios:'',
-            nombre:'',
-            telefono:'',
-            observaciones:'',
-        });
+    const [services, setServices] = useState({ //datos formulario
+        servicios: '',
+        nombre: '',
+        telefono: '',
+        observaciones: '',
+    });
 
 
     const handleChange = (e) => {
         const { name, value } = e.target;
         setServices({ ...services, [name]: value });
     };
-            
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        setFormularioEnviado(true); // Actualiza el estado cuando se envía el formulario
     };
 
-    //configuración calendario:
+    // Configuración calendario:
     const isWeekday = (date) => {
         const day = getDay(date);
         return day !== 0 && day !== 6;
     };
 
-
     // Filtrar las horas permitidas (10:00-14:00 y 16:00-20:00) y restringir horas pasadas
     const filterTime = (time) => {
         const now = new Date();
         const hour = time.getHours();
-        const isInValidRange = (hour >= 10 && hour < 20) 
+        const isInValidRange = hour >= 10 && hour < 20;
         const isInFuture = time > now;
         return isInValidRange && isInFuture;
     };
 
     return (
         <>
-        <div className={styles.reserva}>
-            <h1>Reserva tu tratamiento</h1>
-        </div>
-        <DatePicker
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-            showTimeSelect
-            filterTime={filterTime} //Filtrar las horas permitidas y horas pasadas
-            dateFormat="d, MMMM , yyyy hh:mm aa"
-            withPortal // calendario se abre como en una pantalla
-            timeIntervals={60} // cada 1 hora
-            filterDate={isWeekday} //filtra sólo de lunes a viernes 
-            timeFormat="HH:mm"
-            placeholderText="Escoge tu cita"
-            showIcon//icono calendario
-            toggleCalendarOnIconClick//y que funcione al hacer click
-            //excludeTimes={} //bloquear citas agendadas
-            required
-    
-        />
-        <form onSubmit={handleSubmit}>
-            <div>
-                <select
-                id="servicios"
-                name="servicios"
-                value={setServices.servicios}
-                onChange={handleChange}
-                required>
-                    <option value="">Escoge tu tratamiento</option>
-                    <option value="">Armonización facial</option>
-                    <option value="">Acmella Oleracea</option>
-                    <option value="">Láser CO2 fraccionado</option>
-                    <option value="">Limpieza Facial Detox Skin</option>
-                    <option value="">Limpieza Facia Premium</option>
-                    <option value="">Láser para eliminar estrías</option>
-                    <option value="">Láser para eliminar varices</option>
-                    <option value="">Imperium</option>
-                    <option value="">Zionic</option>
-                </select>
+            <div className={styles.reserva}>
+                <h1>Reserva tu tratamiento</h1>
             </div>
-            <div>
-                <label>Nombre:</label>
-                <input
-                type='text'
-                id='nombre'
-                name='nombre'
-                value={setServices.nombre}
-                onChange={handleChange}
-                required
-                />
-            </div>
-            <div>
-                <label>Teléfono:</label>
-                <input 
-                type= 'tel'
-                id='telefono'
-                name='telefono'
-                value={setServices.telefono}
-                onChange={handleChange}
-                required
-                />
-            </div>
-            <div>
-                <label>Observaciones;</label>
-                <textarea
-                id="observaciones"
-                name="observaciones"
-                value={setServices.observaciones}
-                onChange={handleChange}
-                />
-                <a>Coméntanos cualquier duda</a>
-            </div>
-            
-            <button type="submit">Agenda tu cita</button>
-            </form>  
+
+            {formularioEnviado ? (
+                <p className={styles.mensajeGracias}>
+                    Gracias por confiar en Tova, nos vemos muy pronto.
+                </p>
+            ) : (
+
+            <div className={styles.form}>   
+                <form onSubmit={handleSubmit}>
+                <div className={styles.nombre}>
+                        <label>Nombre:</label>
+                        <input
+                            type="text"
+                            id="nombre"
+                            name="nombre"
+                            value={services.nombre} // Cambio de setServices a services
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className={styles.telefono}>
+                        <label>Teléfono:</label>
+                        <input
+                            type="tel"
+                            id="telefono"
+                            name="telefono"
+                            value={services.telefono} // Cambio de setServices a services
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className={styles.observaciones}>
+                        <label>Coméntanos tus dudas:</label>
+                        <textarea
+                            id="observaciones"
+                            name="observaciones"
+                            value={services.observaciones} // Cambio de setServices a services
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div  >
+                    <DatePicker className={styles.calendario}
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)} // Actualiza startDate cuando cambia la fecha
+                        //showTimeSelect
+                        //filterTime={filterTime} // Filtrar las horas permitidas y horas pasadas
+                        dateFormat="d, MMMM , yyyy hh:mm aa"
+                        withPortal // Calendario se abre como en una pantalla
+                        timeIntervals={60} // Cada 1 hora
+                        filterDate={isWeekday} // Filtra sólo de lunes a viernes
+                        //timeFormat="HH:mm"
+                        placeholderText="Escoge el día"
+                        showIcon // Icono calendario
+                        toggleCalendarOnIconClick // Y que funcione al hacer click
+                        required
+                        locale={es} // en español
+                        />
+                    </div>
+                    <div >
+                        <select className={styles.hora}
+                            id="hora"
+                            name="hora"
+                            value={services.hora}
+                            onChange={handleChange}
+                            required>
+                            <option value="">Escoge la hora</option>
+                            <option value="10:00">10:00</option>
+                            <option value="11:00">11:00</option>
+                            <option value="12:00">12:00</option>
+                            <option value="13:00">13:00</option>
+                            <option value="14:00">14:00</option>
+                            <option value="16:00">16:00</option>
+                            <option value="17:00">17:00</option>
+                            <option value="18:00">18:00</option>
+                            <option value="19:00">19:00</option>
+                        </select>
+                    </div>
+                    <div className={styles.servicios}>
+                        <select
+                            id="servicios"
+                            name="servicios"
+                            value={services.servicios}
+                            onChange={handleChange}
+                            required
+                        >
+                            <option value="">Escoge tu tratamiento</option>
+                            <option value="Armonización facial">Armonización facial</option>
+                            <option value="Acmella Oleracea">Acmella Oleracea</option>
+                            <option value="Láser CO2 fraccionado">Láser CO2 fraccionado</option>
+                            <option value="Limpieza Facial Detox Skin">Limpieza Facial Detox Skin</option>
+                            <option value="Limpieza Facia Premium">Limpieza Facia Premium</option>
+                            <option value="Láser para eliminar estrías">Láser para eliminar estrías</option>
+                            <option value="Láser para eliminar varices">Láser para eliminar varices</option>
+                            <option value="Imperium">Imperium</option>
+                            <option value="Zionic">Zionic</option>
+                        </select>
+                    </div>
+                    
+                    <button type="submit">Agenda tu cita</button>
+                </form>
+            </div>     
+            )}
         </>
     );
 };
 
 export default ReservarOnline;
+
 
