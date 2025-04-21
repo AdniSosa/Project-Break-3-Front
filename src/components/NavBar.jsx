4172bbab41ab23b1b7afc4bebd0d1f83e4dba059
MarianaLGM <mariana_lobeto@hotmail.com>
2025-04-21 21:49:32 +0200

    cambios

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,17 +12,23 @@ const NavBar = () => {
         <div className={styles.banner}>
             <p className={styles.banner1}>¡15 AÑOS CUIDÁNDOTE! DISFRUTA DE UN 15% DE DESCUENTO EN TODOS NUESTROS TRATAMIENTOS</p>
         </div>
+
         <nav className={!userLogged ? styles.navbar : styles.login}>
-            {!userLogged ? <Link to='/inicio'>INICIO</Link> : <Link to='/admin'>INICIO</Link>}
-            <Link to='/tratamientos-faciales'>FACIAL</Link>
-            <Link to='/tratamientos-corporales'>CORPORAL</Link>
-            <Link to='/acmella-oleracea'>ACMELLA OLERACEA</Link>
-            <Link to='/estrias'>ESTRÍAS</Link>
-            <Link to='/contacto'>CONTACTO</Link>
-            {!userLogged ? <Link to='/reservar-online'>RESERVAR ONLINE</Link> : null}
-            {!userLogged ? <div className={styles.regalaTova}><Link to='/regala-tova' >REGALA TOVA</Link></div> : null}
-            {!userLogged ? null : <div><NewButton /><Logout/></div>}
+            <div className={styles.container}>
+                <img src='https://res.cloudinary.com/dljpuje5r/image/upload/v1745252932/Tova_cn1yyn.png'  alt='logo' className={styles.logo}/>
+            </div>
             
+            <div className={styles.container}>
+                {!userLogged ? <Link to='/inicio'>INICIO</Link> : <Link to='/admin'>ADMINISTRADOR</Link>}
+                <Link to='/tratamientos-faciales'>FACIAL</Link>
+                <Link to='/tratamientos-corporales'>CORPORAL</Link>
+                {!userLogged ? <Link to='/acmella-oleracea'>ACMELLA OLERACEA</Link>: null}
+                {!userLogged ? <Link to='/estrias'>ESTRÍAS</Link>: null}
+                {!userLogged ? <Link to='/contacto'>CONTACTO</Link>: null}
+                {!userLogged ? <Link to='/reservar-online'>RESERVA</Link> : null}
+                {!userLogged ? <Link to='/regala-tova' className={styles.regalaTova}>REGALA TOVA</Link> : null}
+                {!userLogged ? null : <div><NewButton/><Logout/></div>}
+            </div>
         </nav>
         
         </>
