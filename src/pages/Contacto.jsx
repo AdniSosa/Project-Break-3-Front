113f2ada65015181d0de31927ef7031c022ae230
AdniSosa <adni.sori@gmail.com>
2025-04-23 23:58:39 +0200

    Responsive desktop

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -79,12 +79,11 @@ const Contacto = () => {
         <div className={styles.map}>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3029.099479891467!2d-3.7005327245071573!3d40.605628744121645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd43d5a84b923aef%3A0x170ff8e309bcdc0f!2sAv.%20de%20la%20Industria%2C%2028760%20Tres%20Cantos%2C%20Madrid!5e0!3m2!1ses!2ses!4v1744187035972!5m2!1ses!2ses"
-            width="600"
             height="350"
             style={{ border: 0 }}
             allowFullScreen=""
             loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade">
+            referrerPolicy="no-referrer-when-downgrade" className={styles.googleMap}>
           </iframe>
         </div>
       </div>
